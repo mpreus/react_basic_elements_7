4de8588c1e8d852d3ceb9a19180c234f440ef4ce
Maciej Preus <mpreus@onet.eu>
2019-12-01 13:33:50 +0100

    Add JSX correction

--- app/app.js
+++ app/app.js
@@ -3,14 +3,11 @@ const UserAge = () => {
 	const year = parseFloat(prompt("Enter year of birth"));   	/* getting user's year of birth*/
 	const thisYear = new Date().getFullYear();				/* getting current year */
 	const userAge = thisYear - year;
-	
-	return (
-		if ( isNaN(userAge) ) {
-			return <h2>Only numbers are allowed</h2>
-		}
 
-		<h2>Your age: {thisYear - year}</h2>
-	)
+	if ( isNaN(userAge) ) {
+		return <h2>Only numbers are allowed</h2>
+	}
+	return <h2>Your age: {thisYear - year}</h2>
 }
 
 ReactDOM.render(
